8345ebd5ebbfadfe00b8f591aba3ded68a7d55f9
Ponochovny <ponochovny@gmail.com>
2021-03-22 16:05:42 +0200

    routes and middlwares

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
 import express from "express"
+import path from 'path'
+import {requestTime, logger} from './middlewares.js'
 
+const __dirname = path.resolve()
+const PORT = process.env.PORT ?? 3000
 const app = express()
 
-app.listen(3000)
+app.use(express.static(path.resolve(__dirname, 'static')))
+app.use(requestTime)
+app.use(logger)
+
+// app.get('/', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, 'static', 'index.html'))
+// })
+// app.get('/features', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, 'static', 'features.html'))
+// })
+app.get('/download', (req, res) => {
+    console.log(req.requestTime)
+    res.download(path.resolve(__dirname, 'static', 'index.html'))
+})
+
+app.listen(PORT, () => {
+    console.log(`Server has been started on port ${PORT}...`)
+})
