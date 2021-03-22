711dc9bf116ae46b9fe26c5cba20ce145a23b983
Ponochovny <ponochovny@gmail.com>
2021-03-22 18:55:15 +0200

    ejs

--- index.js
+++ index.js
@@ -6,9 +6,20 @@ const __dirname = path.resolve()
 const PORT = process.env.PORT ?? 3000
 const app = express()
 
+app.set('view engine', 'ejs')
+app.set('views', path.resolve(__dirname, 'ejs'))
+console.log(app.get('views'))
+
 app.use(express.static(path.resolve(__dirname, 'static')))
 app.use(requestTime)
 app.use(logger)
+
+app.get('/', (req, res) => {
+    res.render('index')
+})
+app.get('/features', (req, res) => {
+    res.render('features')
+})
 
 // app.get('/', (req, res) => {
 //     res.sendFile(path.resolve(__dirname, 'static', 'index.html'))
